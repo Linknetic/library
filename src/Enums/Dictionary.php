811f4cd8420987c7006c486fc1a4ddbc0e4ec86d
Carlo Nicora <carlo.nicora@gmail.com>
2023-02-14 12:00:24 +0100

    fix: add dictionary elements

--- src/Enums/Dictionary.php
+++ src/Enums/Dictionary.php
@@ -6,11 +6,19 @@ use CarloNicora\Minimalism\Interfaces\DictionaryInterface;
 enum Dictionary: string implements DictionaryInterface
 {
     case User='user';
+    case Course='course';
+    case Module='module';
+    case Subject='subject';
+    case CourseType='coursetype';
 
     public function getEndpoint(): string
     {
         return match($this) {
             self::User => 'users',
+            self::Course => 'courses',
+            self::Module => 'modules',
+            self::Subject => 'subjects',
+            self::CourseType => 'coursetypes',
         };
     }
 
@@ -19,6 +27,10 @@ enum Dictionary: string implements DictionaryInterface
     {
         return match($this) {
             self::User => 'userId',
+            self::Course => 'courseId',
+            self::Module => 'moduleId',
+            self::Subject => 'subjectId',
+            self::CourseType => 'courseTypeId',
         };
     }
 
